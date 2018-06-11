8aa7f9856d8bcd076ba0d94e4fdf73769f00e866
nhuquynh14se111 <nhuquynh14se111@gmail.com>
2018-06-11 11:25:11 +0700

    thay đổi backend

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>SHTP - SaiGon HiTech Park</title>
-
+	<link rel="stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/back_end/bootstrap.min.css"> 
 	<link type="text/css" href="css/front-end/frontendcss.css">
 </head>
 <body>
